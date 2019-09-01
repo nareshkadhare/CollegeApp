c37597c7f94ba12623e0a2e91e0196d6a767593d
Naresh Kadhare <nareshkadhare@gmail.com>
2019-09-01 11:34:09 +0530

    Splash Screen Set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  showSplash = true;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -21,7 +25,9 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString("#FFFFFF");      
       this.splashScreen.hide();
+      timer(3400).subscribe(() => this.showSplash = false)
     });
   }
 }
