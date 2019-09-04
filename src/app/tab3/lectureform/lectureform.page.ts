0960eda06c875380b847b3f22caa0b3466f7439d
Naresh Kadhare <nareshkadhare@gmail.com>
2019-09-04 18:47:31 +0530

    Lecture Form Design Chnages

--- src/app/tab3/lectureform/lectureform.page.ts
+++ src/app/tab3/lectureform/lectureform.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from "@ionic/angular";
+import { ActionSheetController,AlertController } from "@ionic/angular";
 
 @Component({
   selector: 'app-lectureform',
@@ -8,18 +8,29 @@ import { ActionSheetController } from "@ionic/angular";
 })
 export class LectureformPage implements OnInit {
 
-  constructor(public actionSheetController: ActionSheetController) {
+  public lectures:Array<number>;
+  public lectureCount = 1;
 
+  constructor(public actionSheetController: ActionSheetController,public alertController:AlertController) {
+    // this.presentAlertRadio();
+  }
+
+  createLectur() {
+    this.lectures = new Array(Number( this.lectureCount));            
   }
 
   ngOnInit() {
+    
+    
   }
 
   async presentActionSheet() {
     const actionSheet = await this.actionSheetController.create({
       header: 'Select Your Subject',
+      cssClass: 'action-sheets-basic-page',
       buttons: [{
         text: 'JAVA',
+        cssClass : 'actionsheet-Share',
         icon: 'bookmarks',
         handler: () => {
           console.log('Delete clicked');
@@ -58,5 +69,42 @@ export class LectureformPage implements OnInit {
       }]
     });
     await actionSheet.present();
+  }
+
+  
+
+  async presentAlertRadio() {
+    const alert = await this.alertController.create({
+      header: 'Number of Lectures?',
+      backdropDismiss : false,
+      inputs: [
+        {
+          name: 'totalLecture',
+          type: 'text',
+          label: 'Radio 1',
+          min:1,
+          max:20,
+          value: 1
+        },
+        
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler:  data => {
+            console.log('Confirm Cancel'+this.lectures);
+          }
+        }, {
+          text: 'Ok',
+          handler:data => {
+            
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
